ee73a70542fd831fe9b2b8052ea1354e486a8497
kamilolejarz@onet.eu <kamilolejarz@onet.eu>
2022-12-05 17:40:15 +0100

    dodanie klasy - Virtualdevice oraz przesylanie danych telemetrycznych

---
+++ Case study - Industrial IoT/AzureDeviceSdk.Device/VirtualDevice.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+using Opc.UaFx;
+using System.Net.Mime;
+using System.Text;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+
+namespace AzureDeviceSdk.Device
+{
+    public class VirtualDevice
+    {
+        private readonly DeviceClient client;
+
+        public VirtualDevice(DeviceClient client)
+        {
+            this.client = client; 
+        }
+
+
+        public async Task sendTelemetryValues(OpcValue[] telemetryValues,string machineId)
+        {
+
+            var data = new
+            {
+                production_status = telemetryValues[0].Value,
+                workorder_id = telemetryValues[1].Value,
+                good_count = telemetryValues[2].Value,
+                bad_count = telemetryValues[3].Value,
+                temperature = telemetryValues[4].Value
+            };
+
+            var dataString = JsonConvert.SerializeObject(data);
+            Message eventMessage = new Message(Encoding.UTF8.GetBytes(dataString));
+            eventMessage.ContentType = MediaTypeNames.Application.Json;
+            eventMessage.ContentEncoding = "utf-8";
+           
+
+            await client.SendEventAsync(eventMessage);
+            Console.WriteLine("Wiadomosc wyslana z maszyny o numerze ID: {0}", machineId);
+        }
+
+
+    }
+    
+
+
+}
--- Case study - Industrial IoT/Case study - Industrial IoT/Program.cs
+++ Case study - Industrial IoT/Case study - Industrial IoT/Program.cs
@@ -1,32 +1,36 @@
 ﻿using Opc.Ua;
 using Opc.UaFx;
 using Opc.UaFx.Client;
+using Microsoft.Azure.Devices.Client;
+using AzureDeviceSdk.Device;
+using Microsoft.Azure.Devices.Client.Exceptions;
+using Case_study___Industrial_IoT.Properties;
 
 internal class Program
 {
-    private static void Main(string[] args)
+    private static async Task Main(string[] args)
     {
 
+        string deviceConnectionString = Resources.connectionString;
         try {
+            //połaczenie z IoT Hub 
+            using var deviceClient = DeviceClient.CreateFromConnectionString(deviceConnectionString, TransportType.Mqtt);
+            await deviceClient.OpenAsync();
+            var device = new VirtualDevice(deviceClient);
+            Console.WriteLine("Connection success!");
 
-            using (var client = new OpcClient("opc.tcp://localhost:4840"))
+            try
             {
-                client.Connect();
-                var node = client.BrowseNode(OpcObjectTypes.ObjectsFolder);
-
-                List<string> listOfIdMachine = new List<string>();
-                findMachinesId(node, listOfIdMachine);
-
-                /*    foreach (string str in listOfIdMachine)
-                    {
-                        Console.WriteLine(str);
-                    }
-        */
-
-                for (int i = 1; i <= listOfIdMachine.Count - 1; i++)
+                //łaczenie z serwerem maszyn  
+                using (var client = new OpcClient("opc.tcp://localhost:4840"))
                 {
-                    OpcReadNode[] data = readData(listOfIdMachine[i]);
+                    client.Connect();
+                    var node = client.BrowseNode(OpcObjectTypes.ObjectsFolder);
+                    List<string> listOfIdMachine = new List<string>();
+                    findMachinesId(node, listOfIdMachine);
 
+                    OpcReadNode[] data = readData(listOfIdMachine[1]);
+                    //show read values 
                     IEnumerable<OpcValue> job = client.ReadNodes(data);
 
                     foreach (var Item in job)
@@ -34,23 +38,61 @@ internal class Program
                         Console.WriteLine(Item.Value);
                     }
 
-                    Console.WriteLine("\n");
+                    OpcValue[] telemetryValues = new OpcValue[5]; 
+                    //odczytywanie wartosci telemetrycznych - production status, workorderId, good, bad, temp 
+                    telemetryValues[0] = client.ReadNode(listOfIdMachine[1] + "/ProductionStatus");
+                    telemetryValues[1] = client.ReadNode(listOfIdMachine[1] + "/WorkorderId");
+                    telemetryValues[2] = client.ReadNode(listOfIdMachine[1] + "/GoodCount");
+                    telemetryValues[3] = client.ReadNode(listOfIdMachine[1] + "/BadCount");
+                    telemetryValues[4] = client.ReadNode(listOfIdMachine[1] + "/Temperature");
+
+                    //test - sending values 
+                    await device.sendTelemetryValues(telemetryValues, listOfIdMachine[1]); 
+
+
+
+
+
+                    /*    foreach (string str in listOfIdMachine)
+                        {
+                            Console.WriteLine(str);
+                        }
+            */
+
+                    /*  for (int i = 1; i <= listOfIdMachine.Count - 1; i++)
+                      {
+                          OpcReadNode[] data = readData(listOfIdMachine[i]);
+
+                          IEnumerable<OpcValue> job = client.ReadNodes(data);
+
+                          foreach (var Item in job)
+                          {
+                              Console.WriteLine(Item.Value);
+                          }
+
+                          Console.WriteLine("\n");
+                      }*/
                 }
+
+            }
+            catch (OpcException e)
+            {
+                Console.WriteLine("Wiadomość błędu: " + e.Message);
             }
 
         }
-        catch (OpcException e)
+        catch (Exception e)
         {
-            Console.WriteLine("Wiadomosc bledu: "+e.Message);
+            Console.WriteLine("Connection failed!" + e.Message);
         }
-        
-        
+
+
         static void findMachinesId(OpcNodeInfo node, List<string> listOfIdMachine, int level = 0)
         {
             if (level == 1)
             {
                 listOfIdMachine.Add(node.NodeId.ToString());
-            } 
+            }
             level++;
             foreach (var childNode in node.Children())
             {
@@ -70,7 +112,11 @@ internal class Program
                 new OpcReadNode(idOfMachine+"/Temperature"),
                 new OpcReadNode(idOfMachine+"/DeviceError"),
             };
-            return commands; 
+
+            return commands;
         }
+
+
+    
     }
 }
